99e2e51cba7309865a0490d74e08af91978c8ff0
Luke-544 <lukesamuels422@gmail.com>
2024-03-17 20:13:39 +0300

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,18 @@
+#for loop
+#a while loop counts for an unlimited number of times
+#a for loop counts for a limited number of times
+
+#lets start with a simple example
+#for i in range(10):
+    #print(i+1)
+
+#for i in range(10,20):
+ #   print(i)
+
+#Let's make a timer app
+import time
+for i in range(10,0,-1):
+    print(i)
+    time.sleep(1)
+
+print("Happy Birthday Samuel!!!!!!!!")
